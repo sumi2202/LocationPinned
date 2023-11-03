acffb30d381bd8fc6977fb579788907699a58afb
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-11-03 08:58:26 -0400

    first commit

---
+++ app/src/main/java/com/example/locationpinned_assignment2_100782723/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.locationpinned_assignment2_100782723;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LocationPinned_Assignment2_100782723"
+include(":app")
